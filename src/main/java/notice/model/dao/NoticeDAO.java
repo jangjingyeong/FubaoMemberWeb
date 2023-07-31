ddcbeb482ac13eff2c38f602147f61dba7066adc
jangjingyeong <jjg070593@gmail.com>
2023-08-01 08:57:26 +0900

    [ADD] 23/07/31 공지사항 게시판 추가 완료
    1. insert.jsp를 통해 글작성
    2. list.jsp를 통해 게시판 목록 출력

---
+++ src/main/java/notice/model/dao/NoticeDAO.java
@@ -0,0 +1,73 @@
+package notice.model.dao;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import notice.model.vo.Notice;
+
+public class NoticeDAO {
+
+	public int insertNotice(Connection conn, Notice notice) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		String query = "INSERT INTO NOTICE_TBL VALUES(SEQ_NOTICENO.NEXTVAL, ?, ?, 'admin', DEFAULT, DEFAULT, DEFAULT)";
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, notice.getNoticeSubject());
+			pstmt.setString(2, notice.getNoticeContent());
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public List<Notice> selectNoticeList(Connection conn) {
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String query = "SELECT * FROM NOTICE_TBL";
+		List<Notice> nList= new ArrayList<Notice>();
+		try {
+			pstmt = conn.prepareStatement(query);
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				Notice notice = rsetToNotice(rset);
+				nList.add(notice);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rset.close();
+				pstmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return nList;
+	}
+
+	private Notice rsetToNotice(ResultSet rset) throws SQLException {
+		Notice notice = new Notice();
+		notice.setNoticeNo(rset.getInt("NOTICE_NO"));
+		notice.setNoticeSubject(rset.getString("NOTICE_SUBJECT"));
+		notice.setNoticeContent(rset.getString("NOTICE_CONTENT"));
+		notice.setNoticeWriter(rset.getString("NOTICE_WRITER"));
+		notice.setNoticeDate(rset.getTimestamp("NOTICE_DATE"));
+		notice.setUpdateDate(rset.getTimestamp("UPDATE_DATE"));
+		notice.setViewCount(rset.getInt("VIEW_COUNT"));
+		return notice;
+	}
+
+	
+
+}
